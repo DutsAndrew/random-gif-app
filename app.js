7ec024fcdbba95c452e1c304ec62cc75c2be8d2c
Andrew Dutson <Dutsandrew@gmail.com>
2022-06-30 13:02:20 -0600

    refactored promise code to async/await function

--- app.js
+++ app.js
@@ -1,30 +1,20 @@
 const img = document.querySelector('img');
 const apiKey = 'https://api.giphy.com/v1/gifs/translate?api_key=2BmRJkYKyfUPozz932Zr7uUhsr5ANcqR';
 const searchTerms = '&s=';
-let searchItem = '';
 const submitButton = document.querySelector('#submit');
   submitButton.addEventListener('click', defineSearchValue);
 
-function defineSearchValue() {
+ async function defineSearchValue() {
   let searchBarValue = document.querySelector('#search-bar').value;
-  searchItem = searchBarValue;
-  let fetchItem = apiKey + searchTerms + searchItem;
+  let searchItem = apiKey + searchTerms + searchBarValue;
 
-  fetch(fetchItem, {mode: 'cors'})
-  .then(function(response) {
-    if (!response.ok) {
-      throw new Error('Network response was not okay');
-    } else {
-      return response.json();
-    }
-  })
-  .then(function(response) {
-    console.log(response.data.images.original.url);
-    img.src = response.data.images.original.url;
-  })
-  .catch((error) => {
-    console.log('Error:', error);
-    alert('search attempt failed, please try again');
-  });
+  try {
+    const response = await fetch(searchItem, {mode: 'cors'})
+    const searchData = await response.json();
+    img.src = searchData.data.images.original.url;
+  } catch (error) {
+    console.error(error);
+    alert('the server could not find what you were looking for, please try again :)');
+  }
 }
 
